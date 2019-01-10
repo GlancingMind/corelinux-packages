c97bf76274066ad60d002790c0b4d531ef8654ad
SirMalloc <faux.dev@outlook.com>
2019-01-10 14:02:49 +0100

    use -e to check also for symbolic links

--- vagrant/vagrant-startup.sh
+++ vagrant/vagrant-startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #vagrant expects ld to be placed in /lib64 otherwise it refuses to start
-if [ ! -f /lib64/ld-linux-x86-64.so.2 ]
+if [ ! -e /lib64/ld-linux-x86-64.so.2 ]
 then
     mkdir -p /lib64
     ln -s /lib/ld-linux-x86-64.so.2 /lib64
